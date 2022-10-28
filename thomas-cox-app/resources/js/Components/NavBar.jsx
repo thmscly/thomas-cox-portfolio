c7492862602e6c978b3043e0318378a30d5cf0d9
T.C. Cox <thmscly@gmail.com>
2022-10-28 11:30:38 -0400

    Removed all vert lines from nav links

--- thomas-cox-app/resources/js/Components/NavBar.jsx
+++ thomas-cox-app/resources/js/Components/NavBar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function NavBar() {
     return (
         <div className="mt-0 m-3 text-green-600 fixed top-0 right-0 px-6 py-4 sm:block bg-black">
-            <a href="/">Home |&nbsp;</a>
-            <a href="/about">About |&nbsp;</a>
-            <a href="/projects">Projects |&nbsp;</a>
+            <a href="/">Home &nbsp;</a>
+            <a href="/about">About &nbsp;</a>
+            <a href="/projects">Projects &nbsp;</a>
             <a href="/socials">Socials &nbsp;</a>
         </div>
     );
