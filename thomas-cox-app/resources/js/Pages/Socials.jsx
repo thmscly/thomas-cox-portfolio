de42b398e98bb951fcb5a4a289b959df9073347d
T.C. Cox <thmscly@gmail.com>
2022-10-28 10:43:30 -0400

    added overflow-hidden to top level div in Socials - fixed background filling mobile scree

--- thomas-cox-app/resources/js/Pages/Socials.jsx
+++ thomas-cox-app/resources/js/Pages/Socials.jsx
@@ -6,7 +6,7 @@ import EmailButton from "@/Components/EmailButton";
 
 function Socials() {
     return (
-        <div className="relative flex items-top justify-center min-h-screen dark:bg-gray-900 sm:items-center sm:pt-0 bg-black">
+        <div className="relative flex items-top justify-center overflow-hidden min-h-screen dark:bg-gray-900 sm:items-center sm:pt-0 bg-black">
             <div className="font-mono text-green-600 text-center max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <NavBar />
                 <div className=" text-6xl m-8 dark:bg-gray-800 overflow-hidden">
