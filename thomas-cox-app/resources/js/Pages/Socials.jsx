10f395124fbba4a41efb066b93a20f2f020a84bb
T.C. Cox <thmscly@gmail.com>
2022-10-29 11:27:26 -0400

    made all bg's black in Socials

--- thomas-cox-app/resources/js/Pages/Socials.jsx
+++ thomas-cox-app/resources/js/Pages/Socials.jsx
@@ -6,17 +6,17 @@ import EmailButton from "@/Components/EmailButton";
 
 function Socials() {
     return (
-        <div className="relative flex items-top justify-center overflow-hidden min-h-screen dark:bg-gray-900 sm:items-center sm:pt-0 bg-black">
+        <div className="relative flex items-top justify-center overflow-hidden min-h-screen sm:items-center sm:pt-0 bg-black">
             <NavBar />
             <div className="font-mono text-green-600 text-center max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <div className=" text-6xl mt-20 m-8 dark:bg-gray-800 overflow-hidden">
+                <div className=" text-6xl mt-20 m-8 overflow-hidden">
                     <h1>Social</h1>
                 </div>
-                <div className="text-3xl m-7 dark:bg-gray-800 overflow-hidden">
+                <div className="text-3xl m-7 overflow-hidden">
                     <h2>Here's how you can find me on the web.</h2>
                 </div>
                 <hr />
-                <div className="grid flex grid-cols-1 gap-4 m-5 sm:bg-blue md:grid-cols-3 sm:grid-cols-3">
+                <div className="grid flex grid-cols-1 gap-4 m-5 md:grid-cols-3 sm:grid-cols-3">
                     <div className="flex-col">
                         <a
                             href="https://www.linkedin.com/in/thomas-tc-cox/"
